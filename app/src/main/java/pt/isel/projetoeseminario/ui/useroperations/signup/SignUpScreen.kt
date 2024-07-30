20c37c0a8e4fce91bb96e205d975376aeccc714d
mikemreira <a46092@alunos.isel.pt>
2024-07-30 21:00:15 +0100

    Delete app directory

--- app/src/main/java/pt/isel/projetoeseminario/ui/useroperations/signup/SignUpScreen.kt
+++
@@ -1,9 +0,0 @@
-package pt.isel.projetoeseminario.ui.useroperations.signup
-
-import androidx.compose.runtime.Composable
-import pt.isel.projetoeseminario.viewModels.UserViewModel
-
-@Composable
-fun SignUpScreen(viewModel: UserViewModel, onLogInClick: () -> Unit) {
-    SignUpForm(viewModel, onLogInClick)
-}
