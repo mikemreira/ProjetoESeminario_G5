67c4c60627438abdc15d82e191d9eb40bfcd4a24
Miguel Moreira <a46092@alunos.isel.pt>
2024-08-29 18:40:09 +0100

    Test both workflows

--- js/projeto/src/App.tsx
+++ js/projeto/src/App.tsx
@@ -17,7 +17,7 @@ import ForgotPassword from "./user/ForgotPassword";
 import ResetPassword from "./user/ResetPassword";
 
 export const path = "https://rest-api.braveforest-4129df1b.northeurope.azurecontainerapps.io"
-//export const path = "/api" abc
+//export const path = "/api"
 
 const AppLayout = () => {
   return (
